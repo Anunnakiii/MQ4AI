33f3daaded4da7d0b94604a19aa573b400df94a1
BoiWong <2809984835@qq.com>
2021-09-08 12:09:03 +0800

    更新 - 重构MQ部分代码，收发使用不同channel

--- MQ_Manager.py
+++ MQ_Manager.py
@@ -11,22 +11,27 @@ import pika
 
 
 class MQ:
-    def __init__(self, m_host, m_port, m_user, m_password, m_queue, m_routing_key, m_exchange="", m_r_queue=None):
+    def __init__(self, m_host, m_port, m_user, m_password, m_queue, m_routing_key, m_exchange, m_r_exchange,
+                 m_r_queue):
         self.host = m_host
         self.port = m_port
         self.user = m_user
         self.password = m_password
         self.exchange = m_exchange
+        self.r_exchange = m_r_exchange
         self.queue = m_queue
         self.r_queue = m_r_queue
         self.routing_key = m_routing_key
         self.is_connected = False
+        self.obj = None
         try:
             credentials = pika.PlainCredentials(self.user, self.password)  # 身份认证
             connection = pika.BlockingConnection(pika.ConnectionParameters(
                 host=self.host, port=self.port, credentials=credentials))
             self.channel = connection.channel()
+            self.r_channel = connection.channel()
             self.channel.queue_declare(queue=self.queue, durable=True)
+            self.r_channel.queue_declare(queue=self.r_queue, durable=True)
             self.is_connected = True
         except Exception as e:
             print(e)
@@ -34,6 +39,8 @@ class MQ:
 
     def callback(self, ch, method, properties, body):
         print(f"From: {self.r_queue} Received: {body.decode()}")
+        self.send(b'{"rev":1}')
+
         # data = json.loads(body)
         # print(type(data), data)
         # TODO   Save data/message to local file first or call Kitsune directly
@@ -41,12 +48,15 @@ class MQ:
     def send(self, m_body):
         if self.is_connected:
             print(f"Send to: {self.queue} Message : {m_body}")
-            self.channel.basic_publish(exchange=self.exchange, routing_key=self.routing_key, body=m_body,
+            self.channel.basic_publish(exchange=self.exchange, routing_key=self.routing_key,
+                                       body=m_body,
                                        properties=pika.BasicProperties(delivery_mode=2, ))
 
-    def rece(self, rec_queue):
+    def rece(self, rec_queue=None, m_obj=None):
+        if m_obj is not None:
+            self.obj = m_obj
         if self.r_queue is None:
             self.r_queue = rec_queue
-        if self.is_connected and self.r_queue != None:
-            self.channel.basic_consume(queue=self.r_queue, on_message_callback=self.callback, auto_ack=True)
-            self.channel.start_consuming()
+        if self.is_connected and self.r_queue is not None:
+            self.r_channel.basic_consume(queue=self.r_queue, on_message_callback=self.callback, auto_ack=True)
+            self.r_channel.start_consuming()
