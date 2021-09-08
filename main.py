d9a4293cf10faa1af9b9a7af25c326dd3f9afe28
BoiWong <2809984835@qq.com>
2021-09-08 12:13:25 +0800

    更新 - 配置文件;修改main.py

--- main.py
+++ main.py
@@ -2,37 +2,30 @@ import json
 import time
 from MQ_Manager import MQ
 from GPU_Info import GPU_Info
+from utils import *
 
 if __name__ == '__main__':
-    # Load config file
-    with open('./CONFIG.json', 'r') as f:
-        _config = json.load(f)
-        print(_config)
-    # Set config
     try:
-        HOST = _config['host']
-        PORT = _config['port']
-        USER = _config['user']
-        PASSWORD = _config['password']
-        EXCHANGE = _config['exchange']
-        QUEUE = _config['queue']
-        ROUTING_KEY = _config['routing_key']
+        AI_HOST, AI_PORT, AI_USER, AI_PASSWORD, AI_QUEUE, AI_ROUTING_KEY, AI_EXCHANGE, AI_R_QUEUE, AI_R_EXCHANGE = load_config(
+            "./AI_CONFIG.json")
+        GPU_HOST, GPU_PORT, GPU_USER, GPU_PASSWORD, GPU_QUEUE, GPU_ROUTING_KEY, GPU_EXCHANGE, GPU_R_QUEUE, GPU_R_EXCHANGE = load_config(
+            "./GPU_CONFIG.json")
     except Exception as e:
-        print(f"Set Config Failed: {e}")
+        print("Load config failed")
         exit(1)
-
     # Init MQ
-    ai_mq = MQ(HOST, PORT, USER, PASSWORD, QUEUE, ROUTING_KEY, EXCHANGE)
-    gpu_mq = MQ(HOST, PORT, USER, PASSWORD, "gpu", "gpu", EXCHANGE)
+    ai_mq = MQ(AI_HOST, AI_PORT, AI_USER, AI_PASSWORD, AI_QUEUE, AI_ROUTING_KEY, AI_EXCHANGE, AI_R_EXCHANGE, AI_R_QUEUE)
+    gpu_mq = MQ(GPU_HOST, GPU_PORT, GPU_USER, GPU_PASSWORD, GPU_QUEUE, GPU_ROUTING_KEY, GPU_EXCHANGE, GPU_R_EXCHANGE,
+                GPU_R_QUEUE)
     gpu_info = GPU_Info(gpu_mq)
-    gpu_info.start()
+    # gpu_info.start()
     # Test - send
     msg = json.dumps(
         {"TIME": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "TYPE": "Str/Json", "DATA": "TEST - Message"})
     ai_mq.send(msg)
     # Run
     try:
-        ai_mq.rece("ai")
+        ai_mq.rece()
     except KeyboardInterrupt as ki:
         print("Exit")
         exit(1)
